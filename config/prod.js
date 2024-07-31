1c24782eff2f55b7c9d75df7b9f21c91acabdbbc
Gigo Dev <guyrick21@gmail.com>
2024-07-31 11:18:53 +0300

    Fix prod config and update build

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://theUser:thePass@cluster0-klgzh.mongodb.net/test?retryWrites=true&w=majority',
+  dbURL: process.env.MONGO_URL || 'mongodb+srv://Guy:Guy12345@cluster0.yy3grzp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
   dbName : process.env.DB_NAME || 'station_db'
+
 }
