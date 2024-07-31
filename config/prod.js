33e24592dc563dc10f7b98e710465898efecd867
Gigo Dev <guyrick21@gmail.com>
2024-07-31 11:35:13 +0300

    removed env

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 export default {
-  dbURL: process.env.MONGO_URL || 'mongodb+srv://Guy:Guy12345@cluster0.yy3grzp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-  dbName : process.env.DB_NAME || 'station_db'
+  dbURL:'mongodb+srv://Guy:Guy12345@cluster0.yy3grzp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+  dbName :'station_db'
 
 }
